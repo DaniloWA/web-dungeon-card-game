ed71ec55aa767d4eaf45b522fefb717c297255cb
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-12-09 16:32:01 +0000

    feat: implemented css header

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className='header'>
-      <nav>
-        <a href="#">Dungeon Card Web</a>
-      </nav>
-    </header>
+    <nav className="header">
+      <div id="trapezoid">
+        <a href="#About">Dungeon Card Game</a>
+        <a href="#startGame">Start</a>
+      </div>
+    </nav>
   )
 }
 
