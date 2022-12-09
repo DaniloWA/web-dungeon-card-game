b17012d8cf5d25ed2a3192fb98f9b2b33233a02f
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-12-09 16:31:49 +0000

    feat: implemented css footer

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <h1>footer</h1>
-    </div>
+    <>
+      <footer className="footer">
+          <div className="waves">
+            <div className="wave" id="wave1"></div>
+            <div className="wave" id="wave2"></div>
+            <div className="wave" id="wave3"></div>
+            <div className="wave" id="wave4"></div>
+          </div>
+          <ul className="menu">
+            <li className="menu__item"><a className="menu__link" href="#">Linkedin</a></li>
+            <li className="menu__item"><a className="menu__link" href="#">Github</a></li>
+            <li className="menu__item"><a className="menu__link" href="#">Portfolio</a></li>
+          </ul>
+          <p>&copy;2022 Danilo Oliveira | All Rights Reserved</p>
+        </footer>
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    </>
   )
 }
 
