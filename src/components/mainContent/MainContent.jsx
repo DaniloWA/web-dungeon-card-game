047105b88a7328cd631b077414e6df0a653e03a8
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-12-09 11:17:10 +0000

    feat: created component MainContent

---
+++ src/components/mainContent/MainContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Game from '../game/Game'
+import './MainContent.css'
+
+
+const MainContent = () => {
+  return (
+    <div className='mainContent'>
+      <Game />
+    </div>
+  )
+}
+
+export default MainContent
