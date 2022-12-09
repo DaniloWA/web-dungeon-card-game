3e263660dbb0418e02e8ca4a562caa8b76d5e671
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-12-09 12:23:51 +0000

    feat: implemented logic for Carditem

--- src/components/cardItem/CardItem.jsx
+++ src/components/cardItem/CardItem.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import './CardItem.css'
 
-const CardItem = () => {
+const CardItem = ({imgAvatar,name ,description}) => {
   return (
-    <div>CardItem</div>
-  )
+    <div className='cardItem'>
+      <img src={imgAvatar} alt="Sample" />
+      <div className='cardItem_title'>
+        <h2>{name}</h2>
+      </div>
+      <div className="cardItem_description">
+        {description}
+      </div>
+    </div>
+   )
 }
 
 export default CardItem
