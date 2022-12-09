540ee3b123b2e8df2f51162130a48ed12045b73d
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-12-09 11:13:48 +0000

    feat: created component AsideBoxLeft

---
+++ src/components/asideBoxLeft/AsideBoxLeft.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const AsideBoxLeft = () => {
+  return (
+    <div className='asideBoxLeft'>
+      <div className='asideBoxLeft_title'>
+        <h1>Histórico</h1>
+      </div>
+      <div className='asideBoxLeft_row'>
+        asideBoxLeft row
+      </div>
+      <div className='asideBoxLeft_row'>
+        asideBoxLeft row
+      </div>
+      <div className='asideBoxLeft_row'>
+        asideBoxLeft row
+      </div>
+      <div className='asideBoxLeft_row'>
+        asideBoxLeft row
+      </div>
+    </div>
+  )
+}
+
+export default AsideBoxLeft
