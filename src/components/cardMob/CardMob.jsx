1ef0218123debaf9d8169b60c71cfd57524b357f
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-12-15 22:34:37 +0000

    feat: implemented visual improvements in CardMob

--- src/components/cardMob/CardMob.jsx
+++ src/components/cardMob/CardMob.jsx
@@ -4,7 +4,7 @@ import './CardMob.css'
 const heartLive = { "src": "/img/heart.png"}
 const heartDie = { "src": "/img/cardiogram.png"}
 
-const CardMob = ({lvl = 1, life = 4, exp = 0, imgAvatar, name, weapon_count = 0, imgWeapon, dmg = 0, description, type}) => {
+const CardMob = ({lvl = 1, life = 4, exp = 0, imgAvatar, name, weapon_count = 0, imgWeapon, dmg = 0, description}) => {
   const hearts = [];
 
   for (let i = 0; i < life; i++) {
@@ -13,6 +13,7 @@ const CardMob = ({lvl = 1, life = 4, exp = 0, imgAvatar, name, weapon_count = 0,
   for (let i = 0; i < 4 - life; i++) {
     hearts.push(<img key={Math.random()} src={heartDie.src} alt="heartLive"/>);
   }
+  
   return (
     <div className='cardMob'>
       <div className='cardMob_header'>
@@ -27,7 +28,9 @@ const CardMob = ({lvl = 1, life = 4, exp = 0, imgAvatar, name, weapon_count = 0,
         </div>
       </div>
       <div className="cardMob_image">
-        <img src={imgAvatar} alt="teste" />
+        <div className="cardMob_image_img">
+          <img src={imgAvatar} alt="teste" />
+        </div>
         <div className="cardMob_image_title">
           <h2>{name}</h2>
         </div>
@@ -44,7 +47,6 @@ const CardMob = ({lvl = 1, life = 4, exp = 0, imgAvatar, name, weapon_count = 0,
         </div>
         <div className="cardMob_description_type">
           <p>{description}</p>
-          <small>{type}</small>
         </div>
       </div>
     </div>
