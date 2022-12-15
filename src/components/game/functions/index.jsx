07f7bac792c91829f96252ad720a7acfd8dae617
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-12-15 23:52:41 +0000

    feat: export functions

---
+++ src/components/game/functions/index.jsx
@@ -0,0 +1,9 @@
+import { shuffleCardBoard } from "./shuffleCardBoard";
+import { setPlayerMovement } from "./setPlayerMovement";
+
+const functions = {
+    shuffleCardBoard,
+    setPlayerMovement,
+}
+
+export default functions
